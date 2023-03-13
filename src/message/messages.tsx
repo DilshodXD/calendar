6ce9cc633da441d4b9633185e5afbc89d50655ee
Dilshod <dilshodxdxd>
2023-03-14 04:45:46 +0500

    Create profile page

--- src/message/messages.tsx
+++ src/message/messages.tsx
@@ -1,10 +1,9 @@
 function Messages() {
-    return (
-      <div>
-        <h1>message page</h1>
-      </div>
-    );
-  }
-  
-  export default Messages;
-  
+  return (
+    <div>
+      <h1>message page</h1>
+    </div>
+  );
+}
+
+export default Messages;
--- src/profile/profile.tsx
+++ src/profile/profile.tsx
@@ -1,9 +1,39 @@
-function Message() {
+import { useEffect, useState } from "react";
+import "./profile.scss";
+import { contentType } from "../interface/interface";
+
+function Profile(): JSX.Element {
+  const Url: string = "https://jsonplaceholder.typicode.com/photos?_limit=5";
+  const [content, setContent] = useState<contentType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data: Response = await fetch(Url);
+      const json: contentType[] = await data.json();
+      setContent(json);
+    };
+    fetchData()
+      .catch(console.error);
+  },[]);
+
   return (
-    <div>
-      <h1>profile page</h1>
+    <div className="home">
+      <div className="container">
+        <div className="home-inner">
+          {content.map((item) => (
+            <div key={item.id} className="content">
+              <img src={item.url} alt={item.title} className="content-img" />
+              <h6 className="content-user">{item.title}</h6>
+              <p className="content-comment">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab,
+                recusandae.
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Message;
+export default Profile;
